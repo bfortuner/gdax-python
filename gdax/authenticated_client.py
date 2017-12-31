361fa88ee6778cc5566afdee3da3e28a83a16f7c
Brendan Fortuner <bfortuner@gmail.com>
2017-12-31 09:35:11 -0800

    removed absolute references

--- gdax/authenticated_client.py
+++ gdax/authenticated_client.py
@@ -11,8 +11,8 @@ import requests
 import base64
 import json
 from requests.auth import AuthBase
-from gdax.public_client import PublicClient
-from gdax.gdax_auth import GdaxAuth
+from .public_client import PublicClient
+from .gdax_auth import GdaxAuth
 
 
 class AuthenticatedClient(PublicClient):
